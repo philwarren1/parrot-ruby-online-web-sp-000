34e6b7ebcc85ce9008799db1c0a399d0922c837f
Philip Warren <philwarren1@gmail.com>
2020-07-21 17:36:18 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -2,10 +2,12 @@
 # returns the phrase
 
 
-def parrot(phrase ="Squawk!")
+def parrot(phrase="Squawk!")
   puts #{phrase}
   return #{phrase}
 end
 
-
+def greeting(name="Ruby programmer", language="Ruby")
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
 
